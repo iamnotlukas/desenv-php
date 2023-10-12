cf402220b8aa26ef8d91692f5d72ff1922b2b405
LukasTheKing <fulturosoldado@gmail.com>
2023-10-12 02:52:37 -0300

    add formatação e organização do código + estilio

--- CRUD-DWEB/CRUD/index.php
+++ CRUD-DWEB/CRUD/index.php
@@ -1,16 +1,29 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tela do Administrador</title>
+  <link rel="stylesheet" href="index.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500&display=swap" rel="stylesheet">
 </head>
 <body>
-   <h1>Tela de Gestão</h1>
-   <a href="addProduto.php">Adicionar Produto</a>
-   <a href="listarProduto.php">Listar Produtos</a>
-   <a href="updateProduto.php">Atualizar Produto</a>
-   <a href="delProduto.php">Deletar Produto</a>
+  <h1>Tela do Administrador</h1>
+  <div class="container">
+    <a href="delProduto.php">
+      <input type="submit" value="Excluir Produto">
+    </a>
+    <a href="addProduto.php">
+      <input type="submit" value="Cadastrar Produto">
+    </a>
+    <a href="listarProduto.php">
+      <input type="submit" value="Listar Produto">
+    </a>
+    <a href="updateProduto.php">
+      <input type="submit" value="Atualizar Produto">
+    </a>
+  </div>
 </body>
 </html>
