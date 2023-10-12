cd619a37752d8b489a827c7abc133bdbd68be0e3
LukasTheKing <fulturosoldado@gmail.com>
2023-10-12 02:52:05 -0300

    alteração na exibição da lista de produtos

--- CRUD-DWEB/CRUD/listarProduto.php
+++ CRUD-DWEB/CRUD/listarProduto.php
@@ -1,5 +1,5 @@
 <h2>LIsta de Produtos</h2>
-
+<link rel="stylesheet" href="index.css">
 <?php
 
 include 'conexao.php';
@@ -10,11 +10,11 @@ $sqlListar = $conn->prepare("SELECT * FROM produto");
 // Executa a consulta
 if ($sqlListar->execute()) {
   while ($produto = $sqlListar->fetch(PDO::FETCH_ASSOC)) {
-    echo '<h2>Nome do Produto: ' . $produto['nomeProduto'] . '</h2>';
-    echo '<p>Id do Produto: ' . $produto['idProduto'] . '</p>';
-    echo '<p>Descrição do Produto: ' . $produto['descricaoProduto'] . '</p>';
-    echo '<p>Valor do Produto: R$ ' . number_format($produto['vlProduto'], 2, ',', '.') . '</p>';
-    echo '<img src="' . $produto['imgLink'] . '" alt="' . $produto['nomeProduto'] . '"><br><br>';
+    echo '<h2>Nome do Produto:<span> ' . $produto['nomeProduto'] . '</span></h2>';
+    echo '<p>Id do Produto: <span> ' . $produto['idProduto'] . '</span></p>';
+    echo '<p>Descrição do Produto:<span> ' . $produto['descricaoProduto'] . '</span></p>';
+    echo '<p>Valor do Produto: <span>R$ ' . number_format($produto['vlProduto'], 2, ',', '.') . '</span></p>';
+    echo '<img style="width: 300px;" src="' . $produto['imgLink'] . '" alt="' . $produto['nomeProduto'] . '"><br><br>';
   }
 } else {
   echo "Não foi possível obter os produtos";
@@ -23,3 +23,7 @@ if ($sqlListar->execute()) {
 $conn = null;
 
 ?>
+
+<a href="index.php">
+    <input type="button" value="Tela de Gestão">
+</a>
