67fca317723dc646790a0f4dcc38d6c0e0c54df8
LukasTheKing <fulturosoldado@gmail.com>
2023-10-12 01:57:39 -0300

    alterando fomratação do código

--- CRUD-DWEB/CRUD/addProduto.php
+++ CRUD-DWEB/CRUD/addProduto.php
@@ -1,53 +1,79 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Adicionar Produto</title>
+  <link rel="stylesheet" href="login.css">
+</head>
 <body>
+    <div class="main-login">
+        <form method="post" action="#">
 
-<h2>Adicionar Produto</h2>
-
-<form action="#" method="post">
-  Nome do Produto:<br>
-  <input type="text" name="nomeProduto" required>
-  <br>-
-  Descrição do Produto:<br>
-  <input type="text" name="descricaoProduto" required>
-  <br>
-  Valor do Produto:<br>
-  <input type="number" name="vlProduto" step=".01" required>
-  <br>
-  Link da Imagem:<br>
-  <input type="text" name="imgLink" required>
-  <br><br>
-  <input type="submit" value="Inserir">
-</form> 
-
-</body>
+          <div class="right-login" style="flex-wrap:wrap;">
+            <div class="card-login">
+              <h1>Adicionar Produto</h1>
+              <div class="textfield">
+                <label>Nome do Produto:</label><br>
+                   <input type="text" name="nomeProduto" required>
+                </div>
+                <div class="textfield">
+                   <label>Descrição do Produto:</label><br>
+                     <input type="text" name="descricaoProduto" required>
+                  </div>
+                  <div class="textfield">
+                  <label>Valor do Produto:</label><br>
+                       <input type="number" name="vlProduto" step=".01" required>
+                  </div>
+                  <div class="textfield">
+                     <label>Link da Imagem:</label><br>
+                        <input type="text" name="imgLink">
+                  </div>
+                  
+                </div>
+                <input type="submit" value="Adicionar" name="btnAdicionar">
+                <br>
+                <a href="index.php">
+                  <input type="button" value="Tela de Gestão">
+                </a>
+                </div>
+            </div>
+          </div>
+        </form>
+      </body>
 </html>
 
 
 <?php
+  if (!empty($_POST['btnAdicionar'])){
+  try{
 
-include 'conexao.php'; 
+  include 'conexao.php'; 
+  
+  $sqlInserir = $conn->prepare("INSERT INTO produto (nomeProduto, descricaoProduto, vlProduto, imgLink) VALUES (:nomeProduto, :descricaoProduto, :vlProduto, :imgLink)");
+  
+    $nomeProduto = $_POST['nomeProduto'];
+    $descricaoProduto = $_POST['descricaoProduto'];
+    $vlProduto = $_POST['vlProduto'];
+    $imgLink = $_POST['imgLink']; // Pega o link da imagem do formulário
+    
+    $sqlInserir->bindParam(':nomeProduto', $nomeProduto);
+    $sqlInserir->bindParam(':descricaoProduto', $descricaoProduto);
+    $sqlInserir->bindParam(':vlProduto', $vlProduto);
+    $sqlInserir->bindParam(':imgLink', $imgLink);
 
-$sqlInserir = $conn->prepare("INSERT INTO produto (nomeProduto, descricaoProduto, vlProduto, imgLink) VALUES (:nomeProduto, :descricaoProduto, :vlProduto, :imgLink)");
-
-$nomeProduto = $_POST['nomeProduto'];
-$descricaoProduto = $_POST['descricaoProduto'];
-$vlProduto = $_POST['vlProduto'];
-$imgLink = $_POST['imgLink']; // Pega o link da imagem do formulário
-
-$sqlInserir->bindParam(':nomeProduto', $nomeProduto);
-$sqlInserir->bindParam(':descricaoProduto', $descricaoProduto);
-$sqlInserir->bindParam(':vlProduto', $vlProduto);
-$sqlInserir->bindParam(':imgLink', $imgLink);
-
-// Executa a consulta
-if ($sqlInserir->execute()) {
-  echo "Novo registro criado com sucesso";
-} else {
-  echo "Não foi possível criar o registro";
+    if ($sqlInserir->execute()) {
+      echo "<script>alert('Produto adicionado com sucesso')</script>";
+    } else {
+      echo "<script>alert('Não foi possível deletar o pro')</script>duto";
+    }
+    // Fechar a conexão com o banco de dados
+    $conn = null;
+  }
+  catch(PDOException $e){
+    echo $sqlInserir . "<br>" . $e->getMessage();
 }
-  // Fechar a conexão com o banco de dados
-  $conn = null;
-?>
+
+}
 
 ?>
