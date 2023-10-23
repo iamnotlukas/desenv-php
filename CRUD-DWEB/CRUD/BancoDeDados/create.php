db0b3e1a6fef9f85d7e77d4c1cd84730f10697af
LukasTheKing <fulturosoldado@gmail.com>
2023-10-23 00:12:20 -0300

    sistema de criação do banco de dados

--- CRUD-DWEB/CRUD/BancoDeDados/create.php
+++ CRUD-DWEB/CRUD/BancoDeDados/create.php
@@ -1,28 +1,51 @@
 <?php
-// Arquivo de criação do sistema
+// Criação do banco de dados
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "sistema";
 
-// Inclui o arquivo de conexão
-include 'conexao.php';
+try {
+  // Cria o banco de dados
+  $sqlCriarDatabase = "CREATE DATABASE IF NOT EXISTS $dbname";
+  $conn = new PDO("mysql:host=$servername", $username, $password); // Conecta sem especificar o banco de dados
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $conn->exec($sqlCriarDatabase);
+  echo "Banco de dados criado com sucesso <br>";
+  $conn = null;
+} catch (PDOException $e) {
+  echo $sqlCriarDatabase . "<br>" . $e->getMessage();
+}
 
-// Cria o banco de dados
-$conn->query("CREATE DATABASE IF NOT EXISTS $dbname");
+// Criação das tabelas
+try {
+  $sqlCriarTabelas = "
+    CREATE TABLE IF NOT EXISTS usuario (
+      id_cli INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+      nome_cli VARCHAR(45) NOT NULL,
+      email_cli VARCHAR(50) NOT NULL,
+      senha_cli VARCHAR(16) NOT NULL
+    );
 
-// Fecha a conexão
-$conn = null;
+    CREATE TABLE IF NOT EXISTS produto (
+      idProduto INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+      nomeProduto varchar(50) NOT NULL,
+      descricaoProduto varchar(255) NOT NULL,
+      vlProduto NUMERIC(10,2) NOT NULL,
+      imgLink VARCHAR(255) NOT NULL
+    );
+  ";
 
-// Cria a tabela "produto"
-$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password); // Conecta ao banco de dados 'sistema'
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $conn->exec($sqlCriarTabelas);
+  echo "Tabelas do banco de dados criadas com sucesso";
+  $conn = null;
 
-$sqlCriarDatabase = $conn->query("CREATE TABLE IF NOT EXISTS produto (
-idProduto INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-nomeProduto varchar(50) NOT NULL,
-descricaoProduto varchar(255) NOT NULL,
-vlProduto NUMERIC(10,2) NOT NULL,
-imgLink VARCHAR(255) NOT NULL
-);");
-
-// Fecha a conexão
-$conn = null;
+  // Redireciona para a página de cadastro
+  header("Location: ../algoritmo/cad.php");
+} catch (PDOException $e) {
+  echo $sqlCriarTabelas . "<br>" . $e->getMessage();
+}
 
 ?>
