232ee3b9884e72c8567ba9915733f9d6b9ed6649
Lucas Andrade Dias <115657720+iamnotlukas@users.noreply.github.com>
2023-10-23 16:31:53 -0300

    adicionado sistema de criação
    Adicionado sistema de criação de banco de dados antes de entrar na página de cadastro

--- CRUD-DWEB/CRUD/algoritmo/cad.php
+++ CRUD-DWEB/CRUD/algoritmo/cad.php
@@ -1,25 +1,21 @@
 <?php
+// Inicia a sessão PHP
+session_start();
 
-//iniciar a sessao php
-if (!file_exists("../BancoDeDados/create.php")){
-    //resireciona para a pagina de criação do banco de dados
-    header("Location: ../BancoDeDados/create.php);
-    exit;
+// Verifica se as tabelas do banco de dados foram criadas
+if (!file_exists("../BancoDeDados/create.php")) {
+  // Redireciona para a página de criação do banco de dados
+  header("Location: ../BancoDeDados/create.php");
+  exit;
 }
 
-if (!isset($_SESSION['usuarioLogin'])){
-
-   //redirecionando para a pagina de login
-   header("Location: login.php");
-   exit;
-
+// Verifica se o usuário está autenticado
+if (!isset($_SESSION['usuarioLogin'])) {
+  // Redireciona para a página de login
+  header("Location: login.php");
+  exit;
 }
-
 ?>
-
-
-
-
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
